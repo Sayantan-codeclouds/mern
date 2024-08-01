b0b1470612ce88c039c80dcca89c330e9c259939
unknown <sayantan.das@codeclouds.com>
2024-08-01 13:23:34 +0530

    SSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import NavBar from "./components/NavBar";
 import Shop from "./pages/Shop";
-import ProductDetails from "./pages/ProductDetails";
-import NavBar from "./components/NavBar"; // Your NavBar component if available
-
-import "./index.css"; // Import global styles if any
-
-// eslint-disable-next-line react-refresh/only-export-components
-const App = () => {
+import ProductDetails from "./pages/Product";
+function App() {
   return (
     <Router>
-      <NavBar /> {/* Optional: Add your navigation bar here */}
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -25,7 +20,6 @@ const App = () => {
       </Routes>
     </Router>
   );
-};
+}
 
-// eslint-disable-next-line react/no-deprecated
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
