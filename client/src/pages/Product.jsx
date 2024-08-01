d5c69dc7d5adc8b24de9a50446d9ef031c812f88
unknown <sayantan.das@codeclouds.com>
2024-08-01 11:36:07 +0530

    SSS

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -15,7 +15,7 @@ const ProductDetails = () => {
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await axios.get(`${apiUrl}api/products/${product_id}`);
+        const response = await axios.get(`${apiUrl}api/products/102`);
         setProduct(response.data);
         setLoading(false);
       } catch (err) {
